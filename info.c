6f6f47410d87ff2d89981207fb53127c2eb55c84
foxler2010 <drew@drewssite.top>
2025-10-30 12:54:15 -0500

    first implementation of "Time of Day (nanoseconds)". Not quite working yet.

--- info.c
+++ info.c
@@ -8,8 +8,52 @@
  * Compile with: make
  */
 
+#define _POSIX_C_SOURCE 199309L
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
 int main(int argc, char* argv[])
 {
-    // TODO
-    return 0;
+    /*
+     * Time of day (nanoseconds)
+     */
+    struct timespec *ts = malloc(sizeof(struct timespec));
+    ts->tv_sec = 0;
+    ts->tv_nsec = 0;
+    clock_gettime(CLOCK_REALTIME, ts);
+    long time = ((long) ts->tv_sec * 10^9) + ts->tv_nsec;
+    printf("Time: %ldns\n", time);
+    free(ts);
+
+    /*
+     * Hostname
+     */
+
+    /*
+     * System name
+     */
+    
+    /*
+     * OS release/version
+     */
+    
+    /*
+     * Hardware type
+     */
+    
+    /*
+     * # of CPUs
+     */
+    
+    /*
+     * Physical memory (in bytes)
+     */
+    
+    /*
+     * Free memory (in bytes)
+     */
+
+    return EXIT_SUCCESS;
 }
