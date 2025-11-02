9906dafbbf0bdfc261ee37bd8b6d6468f3febb0b
foxler2010 <drew@drewssite.top>
2025-11-02 17:16:45 -0600

    implement pmod program

--- pmod.c
+++ pmod.c
@@ -8,8 +8,23 @@
  * Compile with: make
  */
 
+#define _XOPEN_SOURCE
+#define _POSIX_C_SOURCE 199309L
+#define DURATION 1837272638
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
 int main(int argc, char* argv[])
 {
-    // TODO
-    return 0;
+    nice(10);
+    struct timespec *duration = calloc(1, sizeof(struct timespec));
+    duration->tv_sec = DURATION / (int) 1e9;
+    duration->tv_nsec = DURATION % (int) 1e9;
+    nanosleep(duration, duration);
+    free(duration);
+    printf("Goodbye!\n");
+    return EXIT_SUCCESS;
 }
