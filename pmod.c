3589f4435fc84c4e585aaad7ca37d398bc220308
foxler2010 <drew@drewssite.top>
2025-11-02 20:42:24 -0600

    add comments to pmod

--- pmod.c
+++ pmod.c
@@ -19,12 +19,28 @@
 
 int main(int argc, char* argv[])
 {
+    /*
+     * nice()
+     * increments the "niceness" of the calling process by the given amount.
+     * "niceness" is a number that the kernel's scheduler uses along with other factors
+     * to determine how much CPU time to give the process. It is called "niceness" because
+     * the higher the number is, the "nicer" the process is to other processes by being more flexible
+     * with its CPU time requirements.
+     */
     nice(10);
+
+    /*
+     * nanosleep()
+     * puts the process into a sleep state for the given duration of time (in timespec format).
+     * the following code has the process sleep for DURATION nanoseconds (1,837,272,638ns).
+     * that's 1 second and 837,272,638 nanoseconds.
+     */
     struct timespec *duration = calloc(1, sizeof(struct timespec));
-    duration->tv_sec = DURATION / (int) 1e9;
+    duration->tv_sec = DURATION / (int) 1e9; // integer division
     duration->tv_nsec = DURATION % (int) 1e9;
     nanosleep(duration, duration);
     free(duration);
+
     printf("Goodbye!\n");
     return EXIT_SUCCESS;
 }
